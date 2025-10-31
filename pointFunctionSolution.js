f5065c2ff808769889989eaa3bf1ea2ecd36904e
pferna7 <pferna7@uwo.ca>
2025-10-30 23:42:07 -0400

    Add files via upload

---
+++ pointFunctionSolution.js
@@ -0,0 +1,48 @@
+function Point(x, y) {
+    //paste definition from task 1
+
+    this.x = y;
+    this.y = y;
+
+    print: function() {
+        // Use the format specified above
+        console.log("(" + this.x + ", " + this.y + ")");
+    }
+
+
+
+
+    this.midpoint = function(p2){
+        //fill in as specified in README
+        //don't forget to return the point
+
+        let midX = (this.x + p2.x) / 2; // calculate mid point of x
+        let midY = (this.y + p2.y) / 2; // calculate mid point of y
+
+        return new Point(midX, midY);  // return object
+    }
+
+    this.scaleTo = function(p2,t){
+        //fill in as specified in README
+        //don't forget to return the point
+
+        let scaledX = this.x + (p2.x - this.x) * t; // calculate scaled of x
+        let scaledY = this.y + (p2.y - this.y) * t; // calculate scaled of y
+
+        return new Point(scaledX, scaledY);  // return object
+
+    }
+}
+
+
+/* Do not change the lines below */
+let p1 = new Point(1,5)
+let p2 = new Point(5,7)
+p1.midpoint(p2).print()//expect (3, 6)
+p2.midpoint(p1).print()//expect (3, 6)
+
+/* Do not change the lines below */
+let p3 = new Point(0,0)
+let p4 = new Point(4,8)
+p3.scaleTo(p4,0.25).print(1,2)// expect (1, 2)
+p4.scaleTo(p3, 0.75).print() // expect (1, 2)
